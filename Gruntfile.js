91c2b4fbab7ede293f0f20fcf75607a8538b26fa
Dominik Mengelt <dominik.mengelt@gmail.com>
2014-12-20 13:05:59 +0100

    extracted the version handling (same app version in package.json, bower.json and pom.xml) to a seperate maven profile

--- Gruntfile.js
+++ Gruntfile.js
@@ -353,11 +353,8 @@ module.exports = function (grunt) {
               files: ['package.json', 'bower.json'],
               updateConfigs: [],
               commit: false,
-             
               createTag: false,
-            
               push: false,
-             
               globalReplace: false
             }
         }
